7f9b0aa2316a49948db2fdf3f2654a182b3768df
Max <max.white.rabbit@gmail.com>
2018-12-22 22:53:24 +0500

    activity: prtin vec with enumerate

--- formatted_print_display/testcase_list/src/main.rs
+++ formatted_print_display/testcase_list/src/main.rs
@@ -17,7 +17,27 @@ impl fmt::Display for List {
     }
 }
 
+//ACTIVITY
+struct List1(Vec<i32>);
+
+impl fmt::Display for List1 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let vec = & self.0;
+
+        write!(f, "[")?;
+
+        for (count, v) in vec.iter().enumerate() {
+            if count != 0 {write!(f, ", ")?; }
+            write!(f, "{}: {}", count, v)?;
+        }
+
+        write!(f, "]")
+    }
+}
+
 fn main() {
     let v = List(vec![1,2,3]);
-    println!("{}", v);
+    let v1 = List1(vec![1,2,3]);
+    println!("list:\n{}", v);
+    println!("list1:\n{}", v1);
 }
